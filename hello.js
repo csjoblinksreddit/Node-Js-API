922485f578dd73ade6645bec6f2115c7a34022d8
miguel <csjoblinksreddit@gmail.com>
2020-07-29 04:49:12 -0500

    Express

--- hello.js
+++ hello.js
@@ -1,20 +1,7 @@
-// Load HTTP module
-const http = require("http");
+const express = require('express')
+const app = express()
 
-const hostname = "127.0.0.1";
-const port = 8000;
+app.get('/w', (req, res) => res.send('Hello World!'))
 
-// Create HTTP server 
-const server = http.createServer((req, res) => {
 
-   // Set the response HTTP header with HTTP status and Content type
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body "Hello World"
-   res.end('Hello World\n');
-});
-
-// Prints a log once the server starts listening
-server.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
-})
+app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`))
