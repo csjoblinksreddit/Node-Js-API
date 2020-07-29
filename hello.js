70da6a358cd12728d0eb45a3212a1c1201ee78de
csjoblinksreddit <67166406+csjoblinksreddit@users.noreply.github.com>
2020-07-29 04:57:40 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/w', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 
 app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`))
